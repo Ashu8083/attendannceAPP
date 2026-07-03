61123dab5e6218dd208cc54d74ef58c23e2ab23c
Ashu8083n <asutosh.gouda22@gmail.com>
2026-07-04 00:07:07 +0530

    date 3 jul 2026

--- app/middleware/middleware.py
+++ app/middleware/middleware.py
@@ -1,4 +1,28 @@
 import time
+import logging
+from urllib import response
+from urllib.request import Request
 
 
+from starlette.middleware.base import BaseHTTPMiddleware
 
+logger = logging.getLogger(__name__)
+
+class LoggingMiddleWare(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+
+        start = time.time()
+        logger.info(
+            f"{request.method} {request.url.path} started"
+
+        )
+        resonse = await call_next(request)
+        duration = time.time()-start
+
+        logger.info(
+            f"{request.method} {request.url.path}"
+            f"status = {response.status_code}"
+            f"duration = {duration}"
+        )
+
+        return resonse
